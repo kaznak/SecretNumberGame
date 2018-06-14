abc3aa35999e2f3b021b931fdbfe1dfeba652b9d
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2018-06-14 12:42:09 +0900

    feat(cmd): add command line interface

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Lib
+import SecretNumberGame
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Input Seed Number"
+  sn <- doinit
+  putStrLn "Start"
+  checkLoop sn (Hint 0 0)
+
+doinit :: IO SecretNum
+doinit = readLn >>= return . mkSecretNum
+
+checkLoop :: SecretNum -> Hint -> IO ()
+checkLoop sn (Hint 4 0) = do
+  putStrLn "Congratulation!!"
+  print sn
+checkLoop sn _ = do
+  ns <- readLn
+  ht <- return $ checkSecret sn ns
+  print ht
+  checkLoop sn ht
